7f26e51ece7bf59fa531f9d20fa801e689d054e2
Garrett Armstrong <armstrongg@uncw.edu>
2024-11-11 09:54:25 -0500

    uncw change.  pulling svg into app.

--- vivo/installer/webapp/src/main/webapp/themes/wilma_uncw/js/logoHover.js
+++ vivo/installer/webapp/src/main/webapp/themes/wilma_uncw/js/logoHover.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
     var orig = $('.navbar-brand img').attr('src');
     $(".navbar-brand").on("mouseenter", function () {
-        $('.navbar-brand img').attr('src', 'https://uncw.edu/_global/img/uncw-logo-navy_222x222.svg');
+        $('.navbar-brand img').attr('src', '/themes/wilma_uncw/images/uncw-logo-navy_222x222.svg');
     }).on("mouseleave", function () {
         $('.navbar-brand img').attr('src', orig);
     });
