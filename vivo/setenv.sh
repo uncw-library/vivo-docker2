1aa416806c6b5d87d2d3a003f7d90a068e302ca1
Garrett Armstrong <armstrongg@uncw.edu>
2026-09-23 14:08:32 -0400

    renamed to vivo.env

--- vivo/setenv.sh
+++ vivo/setenv.sh
@@ -1,3 +1,3 @@
 # Sourced by catalina.sh. Flags VIVO always needs go here so they survive
-# CATALINA_OPTS / JAVA_OPTS being overridden from .env.
+# CATALINA_OPTS / JAVA_OPTS being overridden from vivo.env.
 CATALINA_OPTS="$CATALINA_OPTS -Dtdb:fileMode=direct"
